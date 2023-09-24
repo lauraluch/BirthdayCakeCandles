e32798a0636be24ce45717c0fafa033a88692493
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-09-24 13:37:41 -0300

    refactor: creating a specific function to get the tallest candle

--- src/main/java/BirthdayCakeCandles.java
+++ src/main/java/BirthdayCakeCandles.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class BirthdayCakeCandles {
-    public static int getAmountOfBlownOutCandles(int age, List<Integer> candleHeights){
+    public static int getTallestCandle(int age, List<Integer> candleHeights) {
         int tallestCandle = candleHeights.get(0);
         for (int counter = 0; counter < age; counter++) {
             if (candleHeights.get(counter) > tallestCandle) {
@@ -9,6 +9,10 @@ public class BirthdayCakeCandles {
             }
         }
 
+        return tallestCandle;
+    }
+    
+    public static int getAmountOfBlownOutCandles(int tallestCandle, int age, List<Integer> candleHeights){
         int amountOfCandlesBlownOut = 0;
         for (int counter = 0; counter < age; counter++) {
             if (candleHeights.get(counter) == tallestCandle) {
