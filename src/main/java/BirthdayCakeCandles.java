e7a90bd33dd71bd0ab1e368170d2e9949b66d02c
Laura Luchiari <95256189+lauraluch@users.noreply.github.com>
2023-09-24 15:30:52 -0300

    feat: creating a method to validate the age

--- src/main/java/BirthdayCakeCandles.java
+++ src/main/java/BirthdayCakeCandles.java
@@ -1,7 +1,15 @@
 import java.util.List;
 
 public class BirthdayCakeCandles {
+
+    public void validateAge(int age) {
+        if (age <= 0){
+            throw new IllegalArgumentException("The age must be more than zero.");
+        }
+    }
+
     public int getTallestCandle(int age, List<Integer> candleHeights) {
+        validateAge(age);
         int tallestCandle = candleHeights.get(0);
         for (int counter = 0; counter < age; counter++) {
             if (candleHeights.get(counter) > tallestCandle) {
@@ -13,6 +21,7 @@ public class BirthdayCakeCandles {
     }
 
     public int getAmountOfBlownOutCandles(int age, List<Integer> candleHeights){
+        validateAge(age);
         int tallestCandle = getTallestCandle(age, candleHeights);
         int amountOfCandlesBlownOut = 0;
         for (int counter = 0; counter < age; counter++) {
